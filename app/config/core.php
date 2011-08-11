aa0c9f0256c5ce71234f15381e61167fb6011d89
dustinvannatter <none@none>
2011-08-11 00:12:23 -0400

    configs

--- app/config/core.php
+++ app/config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9armorylite');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '7685930965745354249674968369453');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
