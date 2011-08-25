ea99d31ef057720f77c0b52dc915b4fd8f903a3b
dustinvannatter <none@none>
2011-08-25 12:04:33 -0400

    api controller

---
+++ app/controllers/api_controller.php
@@ -0,0 +1,47 @@
+<?
+
+	class ApiController extends AppController {
+		var $name = 'Api';
+		var $uses = array('Enchants', 'Reforges', 'Stats');
+		var $helpers = array('Common');
+		var $components = array('Curl');
+		
+		protected $debug = false;
+		
+		function beforeFilter() {
+		  parent::beforeFilter();
+		}
+		
+		function enchant($id) {
+			$data = $this->Enchants->getEnchant($id);
+			if ($data['Enchants']['id']) {
+				echo json_encode($data);				
+			} else {
+				echo '{"status":"nok", "reason":"Enchant not found."}';
+			}
+			exit;
+		}
+		
+		function reforge($id) {
+			$data = $this->Reforges->getReforge($id);
+			if ($data['Reforges']['id']) {
+				echo json_encode($data);				
+			} else {
+				echo '{"status":"nok", "reason":"Reforge not found."}';
+			}
+			exit;
+		}
+
+		function stat($id) {
+			$data = $this->Stats->getStat($id);
+			if ($data['Stats']['id']) {
+				echo json_encode($data);				
+			} else {
+				echo '{"status":"nok", "reason":"Stat not found."}';
+			}
+			exit;
+		}		
+		
+	}
+	
+?>
