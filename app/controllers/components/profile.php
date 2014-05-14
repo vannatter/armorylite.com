b43e3d8943d8be1a5a7bb76461698aeaa696f459
Dustin Vannatter <dustin@dustin.local>
2014-05-14 14:15:06 -0400

    merges

--- app/controllers/components/profile.php
+++ app/controllers/components/profile.php
@@ -223,6 +223,10 @@
 	    			$parsed_xml = & new XML($x);
 					$parsed_xml = Set::reverse($parsed_xml);				
 	
+					echo "<pre>";
+					print_r($parsed_xml);
+					echo "</pre>";
+	
 					if ($parsed_xml['Wowhead']['Item']['id']) {
 						$main_item = $this->Items->addItem($parsed_xml['Wowhead']['Item']['id'], $parsed_xml['Wowhead']['Item']['name'], $parsed_xml['Wowhead']['Item']['quality']['id'], $parsed_xml['Wowhead']['Item']['level'], $parsed_xml['Wowhead']['Item']['icon']['value'], ((@$parsed_xml['Wowhead']['Item']['inventorySlot']['id']) ? $parsed_xml['Wowhead']['Item']['inventorySlot']['id'] : $parsed_xml['Wowhead']['Item']['InventorySlot']['id']));
 						
